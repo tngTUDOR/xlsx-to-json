557957c2f90b1e17655cf82d6b84fa761cb0d59d
Tomas Navarrete <tomas.navarrete@list.lu>
2017-10-11 12:34:26 +0200

    trim tests.

--- test/test_xlsx_to_json.py
+++ test/test_xlsx_to_json.py
@@ -1,15 +1,19 @@
+"""
+Test some functions of the script.
+"""
 from openpyxl import load_workbook
-from xlsx_to_json import *
-import os.path
+from xlsx_to_json import processsheet
 
-TEST_FILE= "sample/ResourceAssessmentSummaryData032011.xlsx"
-EXPECTED_SHEET_NAMES = ['All Summary Data'] 
+TEST_FILE = "sample/ResourceAssessmentSummaryData032011.xlsx"
+EXPECTED_SHEET_NAMES = ['All Summary Data']
 
 def test_processsheet():
+    """
+    Compare that the output has 191 rows of data with 15 cols.
+    """
     workbook = load_workbook(TEST_FILE, read_only=True)
     sheet = EXPECTED_SHEET_NAMES[0]
     data = processsheet(workbook, sheet)
     assert len(data) == 191
-    s = data[0]
-    assert len(s) == 15
-
+    sample = data[0]
+    assert len(sample) == 15
