06d141a0955873373ec20cc23e63297da7a33d52
Tomas Navarrete <tomas.navarrete@list.lu>
2017-10-11 12:29:59 +0200

    Remove useless test.

--- test/test_xlsx_to_json.py
+++ test/test_xlsx_to_json.py
@@ -5,14 +5,6 @@ import os.path
 TEST_FILE= "sample/ResourceAssessmentSummaryData032011.xlsx"
 EXPECTED_SHEET_NAMES = ['All Summary Data'] 
 
-def test_showsheets():
-    """
-    The list of sheets of test file:
-    """
-    workbook = load_workbook(TEST_FILE, read_only=True)
-    assert EXPECTED_SHEET_NAMES == workbook.sheetnames
-
-
 def test_processsheet():
     workbook = load_workbook(TEST_FILE, read_only=True)
     sheet = EXPECTED_SHEET_NAMES[0]
